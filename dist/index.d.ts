962ad713110a7e9962e48077b43d0f85b3a682c1
Ali Morshedzadeh <amorshed051@gmail.com>
2025-03-05 16:11:22 +0330

    Add .gitignore and remove dist directory from version control

--- dist/index.d.ts
+++
@@ -1,4 +0,0 @@
-export * from './types';
-export { createCancellable, createCancellableWithReject } from './cancellable';
-export { withTimeout, withTimeoutFn } from './timeout';
-export { createCancellableWithTimeout, createCancellableWithTimeoutAndReject } from './utils';
